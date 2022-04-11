bc823a2275e195d0add0fc5e37cee7bc5939fb32
John Hios <99095981+JohnHios@users.noreply.github.com>
2022-04-11 09:02:02 +0100

    week05 weekend homework complete

--- week_05/weekend_homework/app.R
+++ week_05/weekend_homework/app.R
@@ -1,48 +1,116 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# Load libraries
 library(shiny)
+library(shinythemes)
+library(tidyverse)
+
+
+game_sales <- CodeClanData::game_sales
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  
+  theme = shinytheme("united"),
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Game Sales Data"),
+    
+    
+    # ADD IN A FLUID ROW WITH WIDGETS HERE
+    fluidRow(
+      
+      #Radio Button inputs for game ratings 
+      column(4,
+             radioButtons('rating_input',
+                          'Rating',
+                          choices = unique(game_sales$rating),
+                          inline = TRUE)
+      ), 
+      
+      #Drop down list inputs for year of release 
+      column(4,
+             selectInput("year_input", 
+                         "Year of Release", 
+                         choices = rev(sort(unique(game_sales$year_of_release))))
+      ),
+      
+      #Add an action button
+      column(4,
+             actionButton("update", "Update dashboard"),
+      )
+    ),
+    
+  HTML("<br><br>"),
+    # ADD IN A FLUID ROW WITH PLOTS HERE
+    # compare the average scores of critics and users for the top 5 publishers.
+    # Bar plots are the most appropriate to present results
+    fluidRow(
+    
+    # Show a plot of the top 5 critic score ratings
+     column(6,
+              plotOutput("critic_rating_barplot")
+       ),
+    
+    # Show a plot of the top 5 user score ratings
+     column(6,
+              plotOutput("user_rating_barplot")
+       )
+    ),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  
+  HTML("<br><br>"),
+    # Show a table of data
+    DT::dataTableOutput("table_output")
+    
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required 
 server <- function(input, output) {
+  
+  
+  filtered_data <- eventReactive(input$update, {
+    game_sales %>%
+      filter(rating == input$rating_input) %>%
+      filter(year_of_release == input$year_input)
+  }) 
+  
+  
+  output$table_output <- DT::renderDataTable({
+    filtered_data()
+  })
+  
+  
+  output$critic_rating_barplot <- renderPlot({
+    filtered_data() %>% 
+      group_by(publisher) %>% 
+      summarise(avg_crit_rating = mean(critic_score), avg_user_rating = mean(user_score)) %>%
+      slice_max(avg_crit_rating, n = 5) %>% 
+      ggplot()+
+      aes(x = reorder(publisher, avg_crit_rating), y = avg_crit_rating)+
+      geom_bar(stat = "identity", position = "dodge", fill = "steelblue") +
+      ggtitle("Average Critic Score Ratings", subtitle = "top 5 publishers") +
+      xlab("Average Critic Score Ratings") +
+      ylab("\nPublisher") +
+      ylim(0, 100) +
+      coord_flip()
+  })
+  
+  
+  output$user_rating_barplot <- renderPlot({
+    filtered_data() %>% 
+      group_by(publisher) %>% 
+      summarise(avg_crit_rating = mean(critic_score), avg_user_rating = mean(user_score)) %>%
+      slice_max(avg_user_rating, n = 5) %>% 
+      ggplot()+
+      aes(x = reorder(publisher, avg_user_rating), y = avg_user_rating)+
+      geom_bar(stat = "identity", position = "dodge", fill = "steelblue") +
+      ggtitle("Average User Score Ratings", subtitle = "top 5 publishers") +
+      xlab("Average User Score Ratings") +
+      ylab("\nPublisher") +
+      ylim(0, 10) +
+      coord_flip()
+  })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
 # Run the application 
